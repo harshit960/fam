bd8244bbbc268e56704dba901920caf2a3c062ac
harshit960 <raj.harshit960@gmail.com>
2025-08-18 20:17:35 +0530

    added video model

--- server/database.py
+++ server/database.py
@@ -2,10 +2,11 @@ import os
 from sqlmodel import create_engine, SQLModel
 from dotenv import load_dotenv
 
+from models import Videos
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://username:password@localhost:5432/database_name")
-
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
--- server/models.py
+++ server/models.py
@@ -3,4 +3,11 @@ from sqlmodel import Field, SQLModel
 
 class Videos(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    video_id: str = Field(index=True, unique=True, nullable=False, max_length=50)
+    video_kind: str = Field(nullable=False, max_length=50)
+    published_at: str = Field(nullable=False) 
+    channel_id: str = Field(nullable=False, max_length=100)
+    title: str = Field(nullable=False)
+    description: Optional[str] = Field(default=None)
+    channel_title: Optional[str] = Field(default=None)
+    thumbnail_url: Optional[str] = Field(default=None)
